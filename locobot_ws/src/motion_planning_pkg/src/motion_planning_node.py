9384c6b1e81b2df5fa1de7533a3d04cea53be752
Kevin Robb <kevin.robb@alumni.ou.edu>
2023-02-03 12:53:38 -0500

    Add test publishing of motion commands.

--- locobot_ws/src/motion_planning_pkg/src/motion_planning_node.py
+++ locobot_ws/src/motion_planning_pkg/src/motion_planning_node.py
@@ -6,24 +6,45 @@ Node to handle ROS interface for getting localization estimate, global map, and 
 
 import rospy
 import numpy as np
+from geometry_msgs.msg import Twist
+from random import random
+from math import pi
 
+# rostopic pub --once /locobot/mobile_base/commands/velocity geometry_msgs/Twist '{linear: {x: 0.5, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.3}}'
 
 ############ GLOBAL VARIABLES ###################
-observation_pub = None
+cmd_pub = None
+test_spd_range = (-1, 1)
+test_ang_range = (-pi/2, pi/2)
 #################################################
 
 # TODO do path planning.
 # TODO do navigation & obstacle avoidance.
 # TODO create control commands & pub them.
 
+def generate_test_command(_msg):
+    """
+    Send a simple twist command to test communication between the ros nodes.
+    """
+    msg = Twist()
+    msg.linear.x = random() * (test_spd_range[1] - test_spd_range[0]) + test_spd_range[0]
+    msg.angular.z = random() * (test_ang_range[1] - test_ang_range[0]) + test_ang_range[0]
+    print("Commanding linear: " + str(msg.linear.x) + ", angular: " + str(msg.angular.z))
+    cmd_pub.publish(msg)
+
 def main():
-    global observation_pub
+    global cmd_pub
     rospy.init_node('motion_planning_node')
 
     # TODO subscribe to localization est.
     # TODO subscribe to or just read the map from file.
 
-    # TODO publish control commands.
+    # Publish control commands.
+    cmd_pub = rospy.Publisher("/locobot/mobile_base/commands/velocity", Twist, queue_size=1)
+
+    # Create a timer.
+    dt = 3 # timer period
+    rospy.Timer(rospy.Duration(dt), generate_test_command)
 
     rospy.spin()
 
