94474a0c30dbc4d7f44eaeb2e3246aaead3bb6f6
Kevin Robb <kevin.robb@alumni.ou.edu>
2023-02-10 12:03:29 -0500

    Read in coarse map, process it, and publish to other nodes.

--- locobot_ws/src/motion_planning_pkg/src/motion_planning_node.py
+++ locobot_ws/src/motion_planning_pkg/src/motion_planning_node.py
@@ -7,15 +7,22 @@ Node to handle ROS interface for getting localization estimate, global map, and 
 import rospy
 import numpy as np
 from geometry_msgs.msg import Twist, Vector3
+from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
 from random import random
 from math import pi
 
 # rostopic pub --once /locobot/mobile_base/commands/velocity geometry_msgs/Twist '{linear: {x: 0.5, y: 0.0, z: 0.0}, angular: {x: 0.0, y: 0.0, z: 0.3}}'
 
 ############ GLOBAL VARIABLES ###################
+bridge = CvBridge()
+map = None # raw global occupancy grid map
 cmd_pub = None
 test_spd_range = (-1, 1)
 test_ang_range = (-pi/2, pi/2)
+# Config parameters. TODO read from a yaml.
+cfg_debug_mode = True
 #################################################
 
 # TODO do path planning.
@@ -39,20 +46,34 @@ def generate_test_command(_msg):
     print("Commanding linear: " + str(msg.linear.x) + ", angular: " + str(msg.angular.z))
     cmd_pub.publish(msg)
 
+def get_map(msg):
+    """
+    Get the global occupancy map to use for path planning.
+    """
+    global map
+    # Convert from ROS Image message to an OpenCV image.
+    occ_map = bridge.imgmsg_to_cv2(msg, desired_encoding='passthrough')
+    # Threshold it to get a binary occupancy grid.
+    # TODO not sure if this is necessary, since it was already an occupancy grid before being sent.
+    if cfg_debug_mode:
+        cv2.imshow("Motion planning node received map", occ_map); cv2.waitKey(0); cv2.destroyAllWindows()
+
 def main():
     global cmd_pub
     rospy.init_node('motion_planning_node')
 
     # Subscribe to localization est.
     rospy.Subscriber("/state/particle_filter", Vector3, get_localization_est, queue_size=1)
-    # TODO subscribe to or just read the map from file.
+    # Subscribe to (or just read the map from) file.
+    rospy.Subscriber("/map/occ", Image, get_map, queue_size=1)
 
     # Publish control commands.
     cmd_pub = rospy.Publisher("/locobot/mobile_base/commands/velocity", Twist, queue_size=1)
 
-    # Create a timer.
-    dt = 3 # timer period
-    rospy.Timer(rospy.Duration(dt), generate_test_command)
+    if cfg_debug_mode:
+        # Create a timer to send test commands every dt seconds.
+        dt = 3 # timer period
+        rospy.Timer(rospy.Duration(dt), generate_test_command)
 
     rospy.spin()
 
--- locobot_ws/src/perception_pkg/src/perception_node.py
+++ locobot_ws/src/perception_pkg/src/perception_node.py
@@ -7,33 +7,112 @@ Node to handle ROS interface for ML model that does the actual observation gener
 import rospy
 import numpy as np
 from sensor_msgs.msg import Image
+import cv2
+from cv_bridge import CvBridge
+
+print("perception node got called")
 
 
 ############ GLOBAL VARIABLES ###################
+bridge = CvBridge()
 observation_pub = None
-# Most recent measurement.
-meas_img = None
-meas_img_height = 0
-meas_img_width = 0
+occ_map_pub = None
+raw_map_pub = None
+most_recent_measurement = None
+# Config parameters. TODO read from a yaml.
+cfg_debug_mode = True
+cfg_map_filepath = "/home/kevin-robb/dev/coarse-map-turtlebot/locobot_ws/src/perception_pkg/config/maps/building2.png"
+cfg_obstacle_balloon_radius_px = 2
 #################################################
 
-# TODO make way to interface with the ML model, which will be in a separate file.
 
-# create a message and publish it.
-def send_state():
+def get_observation_from_model():
+    """
+    Pass the most recent measurement into the ML model, and get back a resulting observation.
+    Publish the observation to be used for localization.
+    """
+    # TODO make way to interface with the ML model, which will be in a separate file.
     msg = Image()
     # publish it.
     observation_pub.publish(msg)
 
-# get measurement from RealSense.
+
 def get_RS_image(msg):
-    global meas_img, meas_img_height, meas_img_width
-    meas_img_height = msg.height
-    meas_img_width = msg.width
-    meas_img = msg.data
+    """
+    Get a measurement Image from the RealSense camera.
+    Could be changed multiple times before the model is ready again, so this allows skipping measurements to prefer recency.
+    """
+    global most_recent_measurement
+    most_recent_measurement = msg
+
+
+def read_coarse_map():
+    """
+    Read the coarse map image from the provided filepath.
+    Save the map itself to use for visualizations.
+    Process the image by converting it to an occupancy grid.
+    I ripped this from my previous personal project since it's pretty general: https://github.com/kevin-robb/live_ekf_slam
+    """
+    # read map image and account for possible white = transparency that cv2 will think is black.
+    # https://stackoverflow.com/questions/31656366/cv2-imread-and-cv2-imshow-return-all-zeros-and-black-image/62985765#62985765
+    img = cv2.imread(cfg_map_filepath, cv2.IMREAD_UNCHANGED)
+    if img.shape[2] == 4: # we have an alpha channel
+        a1 = ~img[:,:,3] # extract and invert that alpha
+        img = cv2.add(cv2.merge([a1,a1,a1,a1]), img) # add up values (with clipping)
+        img = cv2.cvtColor(img, cv2.COLOR_RGBA2RGB) # strip alpha channels
+    if cfg_debug_mode:
+        cv2.imshow('initial map', img); cv2.waitKey(0); cv2.destroyAllWindows()
+
+    # convert from BGR to RGB and save the color map for any viz.
+    raw_map = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # turn this into a grayscale img and then to a binary map.
+    occ_map_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), 200, 255, cv2.THRESH_BINARY)[1]
+    # normalize to range [0,1].
+    occ_map_img = np.divide(occ_map_img, 255)
+    if cfg_debug_mode:
+        cv2.imshow("Thresholded Map", occ_map_img); cv2.waitKey(0); cv2.destroyAllWindows()
+    
+    # consider anything not completely white (1) as occluded (0).
+    occ_map = np.floor(occ_map_img)
+
+    # determine index pairs to select all neighbors when ballooning obstacles.
+    nbrs = []
+    for i in range(-cfg_obstacle_balloon_radius_px, cfg_obstacle_balloon_radius_px+1):
+        for j in range(-cfg_obstacle_balloon_radius_px, cfg_obstacle_balloon_radius_px+1):
+            nbrs.append((i, j))
+    # remove 0,0 which is just the parent cell.
+    nbrs.remove((0,0))
+    # expand all occluded cells outwards.
+    for i in range(len(occ_map)):
+        for j in range(len(occ_map[0])):
+            if occ_map_img[i][j] != 1: # occluded.
+                # mark all neighbors as occluded.
+                for chg in nbrs:
+                    occ_map[max(0, min(i+chg[0], occ_map.shape[0]-1))][max(0, min(j+chg[1], occ_map.shape[1]-1))] = 0
+    occ_map = np.float32(np.array(occ_map))
+    if cfg_debug_mode:
+        cv2.imshow("Ballooned Occ Map", occ_map); cv2.waitKey(0); cv2.destroyAllWindows()
+
+    if cfg_debug_mode:
+        # show value distribution in occ_map.
+        freqs = [0, 0]
+        for i in range(len(occ_map)):
+            for j in range(len(occ_map[0])):
+                if occ_map[i][j] == 0:
+                    freqs[0] += 1
+                else:
+                    freqs[1] += 1
+        print("Occ map value frequencies: "+str(freqs[1])+" free, "+str(freqs[0])+" occluded.")
+
+    # turn them into Image messages to publish for other nodes.
+    bridge = CvBridge()
+    occ_map_pub.publish(bridge.cv2_to_imgmsg(occ_map, encoding="passthrough"))
+    raw_map_pub.publish(bridge.cv2_to_imgmsg(raw_map, encoding="passthrough"))
+
 
 def main():
-    global observation_pub
+    global observation_pub, occ_map_pub, raw_map_pub
     rospy.init_node('perception_node')
 
     # Subscribe to sensor images from RealSense.
@@ -42,6 +121,11 @@ def main():
 
     # Publish refined "observation".
     observation_pub = rospy.Publisher("/observation", Image, queue_size=1)
+
+    # Publish the coarse map for other nodes to use.
+    occ_map_pub = rospy.Publisher("/map/occ", Image, queue_size=1)
+    raw_map_pub = rospy.Publisher("/map/raw", Image, queue_size=1)
+    read_coarse_map()
 
     rospy.spin()
 
