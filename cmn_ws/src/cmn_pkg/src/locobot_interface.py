f5e30e3d1726f78ee3a0711a8d95bdbb07cfde72
Kevin Robb <kevin.robb@alumni.ou.edu>
2023-11-07 13:17:20 -0500

    Use bresenham's line to fill in occupied space.

--- cmn_ws/src/cmn_pkg/src/locobot_interface.py
+++ cmn_ws/src/cmn_pkg/src/locobot_interface.py
@@ -9,6 +9,7 @@ import rospy
 import numpy as np
 import os, cv2
 from sensor_msgs.msg import LaserScan
+from bresenham import bresenham
 
 
 def get_lidar(msg:LaserScan):
@@ -18,8 +19,10 @@ def get_lidar(msg:LaserScan):
     """
     # Use standard size for model predictions, i.e., 128x128 centered on the robot, with 0.01 m/px resolution.
     local_occ_meas = np.zeros((128, 128))
+    resolution = 0.01
     center_r = local_occ_meas.shape[0] // 2
     center_c = local_occ_meas.shape[1] // 2
+    max_range_px = msg.range_max / resolution
     for i in range(len(msg.ranges)):
         # Only use measurements within the valid range.
         if msg.ranges[i] < msg.range_min or msg.ranges[i] > msg.range_max:
@@ -28,12 +31,19 @@ def get_lidar(msg:LaserScan):
         # TODO shift angle based on current robot yaw so this aligns with the global map.
         angle = msg.angle_min + i * msg.angle_increment
         # Convert angle from meters to pixels.
-        dist_px = msg.ranges[i] / 0.01
+        dist_px = msg.ranges[i] / resolution
         # Add the data from this ray's detection to the local occ meas.
-        r = center_r - int(dist_px * np.sin(angle))
-        c = center_c + int(dist_px * np.cos(angle))
-        if r >= 0 and c >= 0 and r < local_occ_meas.shape[0] and c < local_occ_meas.shape[1]:
-            local_occ_meas[r, c] = 1
+        r_hit = center_r - int(dist_px * np.sin(angle))
+        c_hit = center_c + int(dist_px * np.cos(angle))
+        r_max_range = center_r - int(max_range_px * np.sin(angle))
+        c_max_range = center_c + int(max_range_px * np.cos(angle))
+        for cell in bresenham(r_hit, c_hit, r_max_range, c_max_range):
+            # Mark all cells as occupied until leaving the bounds of the image.
+            r = cell[0]; c = cell[1]
+            if r >= 0 and c >= 0 and r < local_occ_meas.shape[0] and c < local_occ_meas.shape[1]:
+                local_occ_meas[r, c] = 1
+            else:
+                break
 
     cv2.namedWindow("LiDAR -> local occ meas", cv2.WINDOW_NORMAL)
     cv2.imshow("LiDAR -> local occ meas", local_occ_meas)
